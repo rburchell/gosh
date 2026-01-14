49d5fa668062d3b952e1b9c9a42342372e7e89cb
Robin Burchell <robin.burchell@crimson.no>
2026-01-14 18:14:11 +0100

    uuid: initial revision

---
+++ uuidv4/uuidv4.go
@@ -0,0 +1,124 @@
+// package uuidv4 is for generating and manipulating UUIDs
+//
+// All UUIDs are V4, RFC 4122 variant.
+//
+// To generate UUIDs, the entry points are May() and Must().
+// They generate the same UUID type, but Must() will panic
+// if generation ever fails (however unlikely that may be).
+package uuidv4
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"github.com/rburchell/gosh/th"
+)
+
+type UUID [16]byte
+
+// Generate a UUID, or return error.
+func May() (UUID, error) {
+	var u UUID
+	if _, err := rand.Read(u[:]); err != nil {
+		return UUID{}, err
+	}
+
+	// set version to 4
+	u[6] = (u[6] & 0x0f) | 0x40
+	// set variant to RFC4122
+	u[8] = (u[8] & 0x3f) | 0x80
+
+	return u, nil
+}
+
+// Generate a UUID, panic if generation failures.
+func Must() UUID {
+	return th.Must(May())
+}
+
+var _ fmt.Stringer = UUID{}
+
+// Returns a string representation of UUID.
+//
+// xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.
+func (u UUID) String() string {
+	var buf [36]byte
+	hex.Encode(buf[0:8], u[0:4])
+	buf[8] = '-'
+	hex.Encode(buf[9:13], u[4:6])
+	buf[13] = '-'
+	hex.Encode(buf[14:18], u[6:8])
+	buf[18] = '-'
+	hex.Encode(buf[19:23], u[8:10])
+	buf[23] = '-'
+	hex.Encode(buf[24:36], u[10:16])
+	return string(buf[:])
+}
+
+// Returns the raw underlying bytes of the UUID.
+//
+// Note that the slice is not copied, so modifying the UUID will also copy the returned slice.
+func (u UUID) Bytes() []byte {
+	return u[:]
+}
+
+// Returns true if the two UUID are equal.
+func (u UUID) Equal(v UUID) bool {
+	return bytes.Equal(u[:], v[:])
+}
+
+// Returns UUID from raw bytes, or error.
+func FromBytes(b []byte) (UUID, error) {
+	if len(b) != 16 {
+		return UUID{}, fmt.Errorf("uuid: invalid length: %d", len(b))
+	}
+	var u UUID
+	copy(u[:], b)
+	return u, nil
+}
+
+// Returns UUID from raw bytes, or panic.
+func MustFromBytes(b []byte) UUID {
+	return th.Must(FromBytes(b))
+}
+
+// Returns UUID parsed from string representation, or error.
+func FromString(s string) (UUID, error) {
+	// TODO: It may make sense to be more permissive in our allowed formats here.
+	if len(s) != 36 ||
+		s[8] != '-' || s[13] != '-' ||
+		s[18] != '-' || s[23] != '-' {
+		return UUID{}, errors.New("uuid: invalid string format")
+	}
+
+	var u UUID
+	_, err := hex.Decode(u[0:4], []byte(s[0:8]))
+	if err != nil {
+		return UUID{}, err
+	}
+	_, err = hex.Decode(u[4:6], []byte(s[9:13]))
+	if err != nil {
+		return UUID{}, err
+	}
+	_, err = hex.Decode(u[6:8], []byte(s[14:18]))
+	if err != nil {
+		return UUID{}, err
+	}
+	_, err = hex.Decode(u[8:10], []byte(s[19:23]))
+	if err != nil {
+		return UUID{}, err
+	}
+	_, err = hex.Decode(u[10:16], []byte(s[24:36]))
+	if err != nil {
+		return UUID{}, err
+	}
+
+	return u, nil
+}
+
+// Returns UUID parsed from string representation, or panic.
+func MustFromString(s string) UUID {
+	return th.Must(FromString(s))
+}
---
+++ uuidv4/uuidv4_test.go
@@ -0,0 +1,138 @@
+package uuidv4
+
+import (
+	"testing"
+)
+
+const uuid1 = "a6075bc7-1a09-443a-b1c0-64de253fb2d6"
+const uuid2 = "7d301ddd-8360-4aa3-9d23-71504d03b6e2"
+
+func TestFromString_Valid(t *testing.T) {
+	u, err := FromString(uuid1)
+	if err != nil {
+		t.Fatalf("expected no err, got %v", err)
+	}
+	if u.String() != uuid1 {
+		t.Fatalf("expected %q, got %q", uuid1, u.String())
+	}
+}
+
+func TestFromString_Uppercase(t *testing.T) {
+	u := MustFromString("7D301DDD-8360-4AA3-9D23-71504D03B6E2")
+	if u.String() != uuid2 {
+		t.Fatalf("expected %q, got %q", uuid1, u.String())
+	}
+}
+
+func TestFromString_Invalid(t *testing.T) {
+	_, err := FromString("not-a-uuid")
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+}
+
+func TestMustFromString(t *testing.T) {
+	u := MustFromString(uuid2)
+	if u.String() != uuid2 {
+		t.Fatalf("expected %q, got %q", uuid2, u.String())
+	}
+}
+
+func TestFromBytes_Valid(t *testing.T) {
+	u1 := MustFromString(uuid1)
+	b := u1.Bytes()
+	u2, err := FromBytes(b)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+	if !u1.Equal(u2) {
+		t.Fatal("UUIDs from string and bytes should be equal")
+	}
+}
+
+func TestFromBytes_Invalid(t *testing.T) {
+	_, err := FromBytes([]byte{1, 2, 3})
+	if err == nil {
+		t.Fatal("expected error for invalid bytes, got nil")
+	}
+}
+
+func TestMustFromBytes(t *testing.T) {
+	u1 := MustFromString(uuid2)
+	u2 := MustFromBytes(u1.Bytes())
+	if !u1.Equal(u2) {
+		t.Fatal("UUIDs from MustFromBytes and original should be equal")
+	}
+}
+
+func TestUUID_Bytes(t *testing.T) {
+	u := MustFromString(uuid1)
+	b := u.Bytes()
+	if len(b) != 16 {
+		t.Fatalf("expected 16 bytes, got %d", len(b))
+	}
+}
+
+func TestUUID_Equal(t *testing.T) {
+	u1 := MustFromString(uuid1)
+	u2 := MustFromString(uuid1)
+	u3 := MustFromString(uuid2)
+	if !u1.Equal(u1) {
+		t.Fatal("equal UUIDs not equal")
+	}
+	if !u1.Equal(u2) {
+		t.Fatal("equal UUIDs not equal")
+	}
+	if u1.Equal(u3) {
+		t.Fatal("unequal UUIDs claimed equal")
+	}
+}
+
+func TestUUID_String(t *testing.T) {
+	u := MustFromString(uuid2)
+	s := u.String()
+	if s != uuid2 {
+		t.Fatalf("expected %q, got %q", uuid2, s)
+	}
+}
+
+func TestMay(t *testing.T) {
+	u, err := May()
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+	s := u.String()
+	if len(s) != 36 {
+		t.Fatalf("expected UUID string length 36, got %d", len(s))
+	}
+
+	// since it's random, we can't do much here except to verify it looks valid-ish.
+	// test version
+	if s[14] != '4' {
+		t.Fatalf("expected version 4, got %v", s[14])
+	}
+	// test variant
+	v := s[19]
+	if v != '8' && v != '9' && v != 'a' && v != 'b' {
+		t.Fatalf("expected RFC4122 variant, got %v", v)
+	}
+}
+
+func TestRandom(t *testing.T) {
+	rands := make(map[string]struct{})
+	for _ = range 100 {
+		u := Must()
+		rands[u.String()] = struct{}{}
+	}
+	if len(rands) != 100 {
+		t.Fatalf("expected 100 unique UUID, only got %d", len(rands))
+	}
+}
+
+func TestMust(t *testing.T) {
+	u := Must()
+	s := u.String()
+	if len(s) != 36 {
+		t.Fatalf("expected UUID string length 36, got %d", len(s))
+	}
+}
